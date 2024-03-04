ffcba8bf72efbc7f9c18674452c41d4c3a704868
bugWare <122195163+Rafaelevora@users.noreply.github.com>
2024-03-04 17:32:06 -0300

    Initial Commit

---
+++ api_sistema_doacao_yr_express/src/index.js
@@ -0,0 +1,33 @@
+const logger = require("./Logger/logger");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Rota executou com sucesso!');
+});
+
+app.get('/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (!isNaN(id)) {
+        res.send(id); 
+        }
+
+        else {
+            throw new Error('Digite apenas numeros')
+        }
+    }
+    catch (error) {
+        logger.error(error.message)
+        res.status(400).send('Requisição inválida')
+    }
+});
+
+app.listen(port, () => {
+    logger.info('Servidor rodando em http://localhost:' + port);
+});
+
+
+//https://blog.4linux.com.br/como-organizar-e-manipular-rotas-com-node-js-e-express/
