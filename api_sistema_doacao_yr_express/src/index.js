f7ecbff793f66cd6febc00c8812709db29e80126
bugWare <122195163+Rafaelevora@users.noreply.github.com>
2024-03-05 10:25:09 -0300

    Fix index.js routes

--- api_sistema_doacao_yr_express/src/index.js
+++ api_sistema_doacao_yr_express/src/index.js
@@ -16,12 +16,29 @@ app.get('/:id', (req, res) => {
         }
 
         else {
-            throw new Error('Digite apenas numeros')
+            throw new Error('Digite apenas numeros');
         }
     }
     catch (error) {
+        logger.error(error.message);
+        res.status(400).send('Requisição inválida');
+    }
+});
+
+app.get('/:value/:quant', (req, res) => {
+    try{
+        const value = req.params.value;
+        const quant = req.params.quant;
+        if (!isNaN(value) && !isNaN(quant)){
+            res.send("Rota executou com sucesso recebendo o valor: " + value + " e quantidade: " + quant + "!");
+        }
+        else{
+            throw new Error('Digite apenas numeros');
+        }
+    }
+    catch(error){
         logger.error(error.message)
-        res.status(400).send('Requisição inválida')
+        res.status(400).send('Requisição inválida');
     }
 });
 
